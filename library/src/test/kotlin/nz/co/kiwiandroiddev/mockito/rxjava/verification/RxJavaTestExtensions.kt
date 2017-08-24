7c71e47f8400283cec3fc8c261ecaa10ed547ad4
Matthew Clarke <matthew.clarke@orionhealth.com>
2017-08-24 17:41:18 +1200

    Minor formatting fix

--- library/src/test/kotlin/nz/co/kiwiandroiddev/mockito/rxjava/verification/RxJavaTestExtensions.kt
+++ library/src/test/kotlin/nz/co/kiwiandroiddev/mockito/rxjava/verification/RxJavaTestExtensions.kt
@@ -7,13 +7,13 @@ import rx.observers.TestSubscriber
 /**
  * Created by matthewcl on 22/08/17.
  */
-fun <T> Observable<T>.subscribeTestSubscriber() : TestSubscriber<T> {
+fun <T> Observable<T>.subscribeTestSubscriber(): TestSubscriber<T> {
     val testSubscriber = TestSubscriber.create<T>()
     this.subscribe(testSubscriber)
     return testSubscriber
 }
 
-fun Completable.subscribeTestSubscriber() : TestSubscriber<Unit> {
+fun Completable.subscribeTestSubscriber(): TestSubscriber<Unit> {
     val testSubscriber = TestSubscriber.create<Unit>()
     this.subscribe(testSubscriber)
     return testSubscriber
